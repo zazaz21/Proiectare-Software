13939bddbb938f01c0bbb182e7a8c23fb2c07186
Marcu Eliza Elena <ellen_elena2000@yahoo.com>
2023-04-01 23:27:06 +0300

    Tema 2_3

--- src/main/java/com/application/repos/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.repos;
-
-//import net.javaguides.springboot.model.Employee;
-import com.application.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // all crud database methods
-}
