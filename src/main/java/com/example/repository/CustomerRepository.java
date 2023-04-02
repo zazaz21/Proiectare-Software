28457e796b7e2c86de03a6f3fd063ebb5fd08e3f
Marcu Eliza Elena <ellen_elena2000@yahoo.com>
2023-04-02 15:34:12 +0300

    Tema_2_12

---
+++ src/main/java/com/example/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+import com.example.dto.OrderResponse;
+import com.example.dto.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+    @Query("SELECT new com.javatechie.jpa.dto.OrderResponse(c.name , p.productName) FROM Customer c JOIN c.products p")
+    public List<OrderResponse> getJoinInformation();
+}
