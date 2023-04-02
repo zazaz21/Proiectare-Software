beb7db7a3e0991104d42fd1dc7da3412bd97d7da
Marcu Eliza Elena <ellen_elena2000@yahoo.com>
2023-04-02 15:42:33 +0300

    Tema_2_14

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.example.repository;
 
-public class ProductRepository {
+import com.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
